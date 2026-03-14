50adbfc402f6ca1684dd50c00bc481699b08bb32
Renat <renatdasania8@gmail.com>
2026-03-14 19:07:51 +0300

    feat: добавить Layout обёртку и настроить react-router маршруты

---
+++ client/src/components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import styles from './Layout.module.css'
+
+export default function Layout({ children }) {
+    return (
+        <div className={styles.layout}>
+            <Header />
+            <main className={styles.main}>
+                {children}
+            </main>
+            <Footer />
+        </div>
+    )
+}
